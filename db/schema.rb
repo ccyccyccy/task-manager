7bdc2cb3e7bfc344c82bdf2a016ce677745586b8
Chee Yuan <chong-1997@hotmail.com>
2019-01-04 13:48:31 +0800

    resetted database, added user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_28_131732) do
+ActiveRecord::Schema.define(version: 2019_01_04_054412) do
 
   create_table "tags", force: :cascade do |t|
     t.string "name"
@@ -26,6 +26,14 @@ ActiveRecord::Schema.define(version: 2018_12_28_131732) do
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.text "remarks"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_tasks_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
